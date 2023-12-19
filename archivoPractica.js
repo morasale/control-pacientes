9a778df2e7fcd13b4578b1be39675c4e9f24599e
Alejandra Mora <esther16mora@gmail.com>
2023-12-19 10:32:58 -0400

    solucion de problema de lectura de inputs

--- archivoPractica.js
+++ archivoPractica.js
@@ -1,16 +1,15 @@
 const h1 = document.querySelector('h1');
-const nombre = document.querySelector('.nombre');
-const apellido = document.querySelector('.apellido');
-const edad = document.querySelector('.edad');
-const cedulaDeIdentidad = document.querySelector('.cedula');
-const direccion = document.querySelector('.direccion');
-const btnSiguiente = document.querySelector('.siguiente');
-
-function leerInput() {
-    console.log("el imput ha cambiado")
-}
+const nombre = document.querySelector('#nombres');
+const apellido = document.querySelector('#apellidos');
+const edad = document.querySelector('#edad');
+const cedulaDeIdentidad = document.querySelector('#cedula');
+const direccion = document.querySelector('#direccion');
+const btnSiguiente = document.querySelector('#siguiente');
+const datosCompletos = document.querySelector('#datosCompletos');
 
 function escucharEvento() {
-    // btnSiguiente.addEventListener('click', btnSiguienteOnClick);
-    console.log('hicieron click');
+    // console.log('hicieron click');
+    const sumaImputs = nombre.value + ' ' + apellido.value + ' ' + edad.value + ' ' + cedulaDeIdentidad.value + ' ' + direccion.value
+    datosCompletos.innerText = 'resultados: ' + sumaImputs;
+  
 }
