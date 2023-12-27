773c4d250b1ce8922af26a2f13267be33a37faba
Alejandra Mora <esther16mora@gmail.com>
2023-12-27 11:10:55 -0400

    solucion de problema de lectura de inputs

--- archivoPractica.js
+++ archivoPractica.js
@@ -9,7 +9,7 @@ const datosCompletos = document.querySelector('#datosCompletos');
 
 function escucharEvento() {
     // console.log('hicieron click');
-    const sumaImputs = nombre.value + ' ' + apellido.value + ' ' + edad.value + ' ' + cedulaDeIdentidad.value + ' ' + direccion.value
+    const sumaImputs = 'Nombre y apellidos del paciente ' + nombre.value + ' ' + apellido.value + ', edad ' + edad.value + ', cedula de identidad ' + cedulaDeIdentidad.value + ', direccion ' + direccion.value
     datosCompletos.innerText = 'resultados: ' + sumaImputs;
   
 }
